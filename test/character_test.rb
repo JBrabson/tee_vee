e01e115e7d048f2cbe01a6e06fc9743a7f94c238
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2020-12-15 09:17:02 -0700

    Add salary test; passing. Iteration 1 complete.

--- test/character_test.rb
+++ test/character_test.rb
@@ -13,4 +13,9 @@ class CharacterTest < Minitest::Test
     assert_equal "KITT", kitt.name
     assert_equal "William Daniels", kitt.actor
   end
+
+  def test_it_has_salary
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    assert_equal 1000000, kitt.salary
+  end
 end
