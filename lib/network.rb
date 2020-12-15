2b3a1011cb89ddb5c1fcc7d5dc699f23d1ec9e46
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2020-12-15 10:27:01 -0700

    Add add_show method and test; passing.

--- lib/network.rb
+++ lib/network.rb
@@ -1,9 +1,13 @@
 class Network
   attr_reader :name,
               :shows
-              
+
   def initialize(name)
     @name = name
     @shows = []
+  end
+
+  def add_show(show_name)
+    @shows << show_name
   end
 end
